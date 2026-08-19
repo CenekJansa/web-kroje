f2cde832005b016289627a98fbfc99fd9cf7db96
radi mek <radim_cech@seznam.cz>
2026-08-19 20:05:43 +0200

    nevim xd

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,9 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
+  nitro: {
+    preset: 'cloudflare-pages',
+  },
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
   vite: {
